68dcf102a0c43fe49b3e3362333606802ddded3b
Wizer27 <svd2yar@gmail.com>
2026-03-08 21:44:20 +0300

    security fix

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends,HTTPException,Request,FastAPI,Header,status
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from pydantic import BaseModel
 from config import PROJECT_ROOT
 import uvicorn
@@ -9,7 +10,9 @@ import asyncio
 import os
 from dotenv import load_dotenv
 import time
-from backend.database.main_core import register,login
+from backend.database.main_core import register,login,get_user_free_trial_end_date,set_sub_to_something,is_user_subbed
+from slowapi import Limiter,_rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
 
 
 load_dotenv()
@@ -17,6 +20,10 @@ load_dotenv()
 
 
 app = FastAPI()
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(429, _rate_limit_exceeded_handler)
+app.add_middleware(HTTPSRedirectMiddleware)
 
 
 
@@ -53,6 +60,8 @@ class AuthorizeData(BaseModel):
     username:str
     password:str
     
+
+    
 @app.post("/register")
 async def register_api(req:AuthorizeData,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
@@ -71,10 +80,17 @@ async def login_api(req:AuthorizeData,x_signature:str = Header(...),x_timestamp:
     try:
         try_reg:bool = await login(req.username,req.password)
         if not try_reg:
-            raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = "User already existst")
+            raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = "Wrong data")
     except Exception as e:
         raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,detail = f"Server error : {e}")
-    
+
+class UsernameOnly(BaseModel):
+    username:str
+
+@app.post("/get/{username}/date",dependencies=[Depends(safe_get)])
+async def get_user_date_end_api(username:str):
+    pass
+
 
 
 
